b7dd1f06a8788208ffcbab9c2969539bc6e01eaf
jonysins2211 <Jonysins11220@gmail.com>
2025-05-25 23:51:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -124,7 +124,7 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else "https://{}/".format(FQDN, PORT)
+URL = "https://movie-loverzz-f2c1e32890d7.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else "https://movie-loverzz-f2c1e32890d7.herokuapp.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'SilentXBotz'))
